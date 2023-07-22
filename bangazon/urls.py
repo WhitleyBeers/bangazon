ce7dc8f1a76cc02549c85daa6c6724d9ac9efb09
Whitley Beers <whitleybeers@gmail.com>
2023-07-22 09:42:12 -0500

    category serializer and view added, categories added to urls, retrieve method tested in postman

--- bangazon/urls.py
+++ bangazon/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from bangazonapi.views import UserView, OrderView
+from bangazonapi.views import UserView, OrderView, CategoryView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'users', UserView, 'user')
 router.register(r'orders', OrderView, 'order')
+router.register(r'categories', CategoryView, 'category')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- bangazonapi/serializers/__init__.py
+++ bangazonapi/serializers/__init__.py
@@ -1,2 +1,3 @@
 from .user_serializer import UserSerializer, CreateUserSerializer
 from .order_serializer import OrderSerializer
+from .category_serializer import CategorySerializer
